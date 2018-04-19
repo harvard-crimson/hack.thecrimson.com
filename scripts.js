59d035224f340b011ff7b90261eb9d9474c9cf0b
Theodore Liu <theodoretliu@gmail.com>
2018-04-19 17:12:12 -0400

    Preliminary final?

--- scripts.js
+++ scripts.js
@@ -5,3 +5,11 @@ function scrollToSection(name) {
     inline: 'nearest'
   });
 }
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() !== 0) {
+    $('.navbar').addClass('scrolled');
+  } else {
+    $('.navbar').removeClass('scrolled');
+  }
+});
